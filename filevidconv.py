74a1138d5c077ce948d42fe170bbeb0386a30c78
RodionGromo <rodion.gromo@gmail.com>
2023-11-14 21:50:51 +0300

    added gif2textfile and renamed file to textfile

--- filevidconv.py
+++ filevidconv.py
@@ -39,8 +39,7 @@ def colorsToSingleImage(colors: list, imagesize: tuple=None):
 def file2img(filename, imagesize: tuple=None):
 	return colorsToSingleImage(byteHexToHexColor(readBytesToHex(open(filename,"r"))), imagesize)
 
-def file2vid(filename, videosize: tuple, framerate=24, output_filename='output.gif'):
-	msDelay = int((1/framerate) * 1000)
+def file2gif(filename, videosize: tuple, frameDurationMS=24, output_filename='output.gif'):
 	colors = byteHexToHexColor(readBytesToHex(open(filename, "r")))
 	frameCount = int(len(colors) / (videosize[0] * videosize[1])) + 1
 	image_list = [Image.new("RGB", videosize) for i in range(frameCount if frameCount > 0 else 1)]
@@ -55,7 +54,7 @@ def file2vid(filename, videosize: tuple, framerate=24, output_filename='output.g
 		except IndexError:
 			break
 		current_il_index += 1
-	image_list[0].save(output_filename, save_all=True, append_images=image_list[1:], duration=msDelay)
+	image_list[0].save(output_filename, save_all=True, append_images=image_list[1:], duration=frameDurationMS)
 	return True
 
 def rgb2hex(rgb):
@@ -75,10 +74,29 @@ def imageToHex(filename):
 				colors += list(color)
 	return colors
 
+def gifFrameToHex(frame):
+	img = frame.convert("RGB")
+	colors = []
+	for y in range(img.size[1]):
+		for x in range(img.size[0]):
+			color = list(rgb2hex(img.getpixel((x,y))))
+			color = [i for i in color if i != "0"]
+			if not isEmptyPixel(color):
+				colors += list(color)
+	return colors
+
 def colorsToText(colors: list, output_filename: str="output.txt"):
 	text = "".join([chr(int(i, 16)) for i in colors]).replace(r"\x00", "")
 	return text
 
-def image2file(filename: str, output_filename: str="output.txt"):
+def image2textfile(filename: str, output_filename: str="output.txt"):
 	with open(output_filename,"w") as file:
 		file.write(colorsToText(imageToHex(filename)))
+
+def gif2textfile(filename: str, output_filename: str="output.txt"):
+	text = ""
+	gif = Image.open(filename)
+	for i in range(gif.n_frames):
+		gif.seek(i)
+		text += colorsToText(gifFrameToHex(gif))
+	return text
